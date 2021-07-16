226cdc5944421a11d300aa505bafd03e46b4f12f
Bruno Moraes <bjsmoraes@gmail.com>
2021-07-16 03:49:59 -0300

    Novo MaterialColor utilizando as cores da marca

--- lib/config/themes/app_themes.dart
+++ lib/config/themes/app_themes.dart
@@ -2,12 +2,33 @@ import 'package:adotappet/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// Create a custom material color (adotappet orange)
+MaterialColor createMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  final swatch = <int, Color>{};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
+}
+
 class AppThemes {
   static ThemeData getThemeData(context) {
     return new ThemeData(
       brightness: Brightness.light,
       primaryColor: AppColors.primary,
-      primarySwatch: Colors.deepOrange,
+      primarySwatch: createMaterialColor(Color(0xFFFB7E2C)),
       textTheme: GoogleFonts.dosisTextTheme(
         Theme.of(context).textTheme,
       ),
